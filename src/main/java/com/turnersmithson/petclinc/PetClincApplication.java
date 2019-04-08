63e1593b88740224431b42761c5ef2922b236c06
iturner <ian.turner@arris.com>
2019-04-08 10:48:15 +0100

    Initial commit

---
+++ src/main/java/com/turnersmithson/petclinc/PetClincApplication.java
@@ -0,0 +1,13 @@
+package com.turnersmithson.petclinc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClincApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClincApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/turnersmithson/petclinc/PetClincApplicationTests.java
@@ -0,0 +1,16 @@
+package com.turnersmithson.petclinc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PetClincApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
