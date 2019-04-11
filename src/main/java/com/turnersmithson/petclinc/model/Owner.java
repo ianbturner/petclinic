234b9e29dbd96ac6a0da1a44b75e89f409368ef5
Ian Turner <ian.turner@arris.com>
2019-04-11 10:03:28 +0100

    Creating the basic POJO Closes #2

---
+++ src/main/java/com/turnersmithson/petclinc/model/Owner.java
@@ -0,0 +1,4 @@
+package com.turnersmithson.petclinc.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/turnersmithson/petclinc/model/Vet.java
@@ -0,0 +1,4 @@
+package com.turnersmithson.petclinc.model;
+
+public class Vet extends Person {
+}
